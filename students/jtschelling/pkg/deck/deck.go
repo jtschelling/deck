25cae83c29c7dac890fae8a90402df96da4ddd8d
jtschelling <jt@schelling.io>
2020-09-12 16:47:59 -0500

    new deck with jokers implemented, random shuffle implemented

--- students/jtschelling/pkg/deck/deck.go
+++ students/jtschelling/pkg/deck/deck.go
@@ -1,33 +1,73 @@
 package deck
 
-// import (
-//
-// )
+import (
+  "math/rand"
+  "time"
+)
 
 type Card struct {
-  value Int
-  suit String
+  Value int
+  Suit string
 }
 
-var suits = []string{
+var cardTypes = [13]string{
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+}
+
+var StandardDeckSize = 52
+
+var Suits = [4]string{
   "club",
   "diamond",
   "heart",
   "spade",
 }
 
-func New() []Card (
-  deck := make([]Card, 52)
+func New(numJokers int) []Card {
+  deck := make([]Card, (StandardDeckSize + numJokers))
 
-  for _, suit := range suits {
-    for i, i <= 13, i++ {
-      card :=
-      deck = append(deck, Card {
-        value: i,
-        suit: suit,
-      })
+  cardPosition := 0
+
+  for _, suit := range Suits {
+    for i := 0; i <= 12; i++ {
+      deck[cardPosition] = Card {
+        Value: i,
+        Suit: suit,
+      }
+      cardPosition++
+    }
+  }
+
+  for i := 0; i < numJokers; i++ {
+    deck[StandardDeckSize + i] = Card {
+      Value: -1,
+      Suit: "",
     }
   }
 
   return deck
-)
+}
+
+func Shuffle(deck []Card, style string) []Card {
+  switch style {
+  case "random":
+    rand.Seed(time.Now().UnixNano())
+    rand.Shuffle(len(deck), func(i, j int) {
+      deck[i], deck[j] = deck[j], deck[i]
+    })
+  }
+
+  return deck
+}
--- students/jtschelling/test/deck_test.go
+++ students/jtschelling/test/deck_test.go
@@ -5,10 +5,92 @@ import (
   "github.com/jtschelling/deck/students/jtschelling/pkg/deck"
 )
 
-func New(t *testing.T) {
-  newDeck := deck.New()
+func testSortedDeck(testDeck []deck.Card) bool {
+  expectedSuit  := 0
+  expectedValue := 0
 
-  if len(newDeck) != 52 {
-    t.Errorf("Deck is not 52 cards")
+  for _, card := range testDeck {
+    if (card.Suit != deck.Suits[expectedSuit] || card.Value != expectedValue) {
+      return false
+    }
+    expectedValue = (expectedValue + 1) % 13
+
+    // move to next suit if expectedValue wrapped around to 0
+    if expectedValue == 0 {
+      expectedSuit++
+
+      // if jokers are in the deck we need to break when all the standard suits
+      // have been exhausted
+      if expectedSuit == 4 {
+        break
+      }
+    }
+  }
+
+  return true
+}
+
+func TestNew(t *testing.T) {
+  numJokers := 0
+  newDeck := deck.New(numJokers)
+
+  if len(newDeck) != deck.StandardDeckSize + numJokers {
+    t.Errorf("Deck is not %d cards", deck.StandardDeckSize + numJokers)
+  }
+
+  if testSortedDeck(newDeck) == false {
+    t.Errorf("New unsorted deck is not in the correct order.")
+  }
+}
+
+func TestNewWithJokers(t *testing.T) {
+  numJokers := 5
+  newDeck := deck.New(numJokers)
+
+  if len(newDeck) != (deck.StandardDeckSize + numJokers) {
+    t.Errorf("Deck is not %d cards", (deck.StandardDeckSize + numJokers))
+  }
+
+  if testSortedDeck(newDeck) == false {
+    t.Errorf("New unsorted deck is not in the correct order.")
+  }
+
+  for i := 0; i < numJokers; i++ {
+    if (newDeck[deck.StandardDeckSize + i].Suit != "" || newDeck[deck.StandardDeckSize + i].Value != -1) {
+      t.Errorf("New unsorted deck does not contain jokers or are in the incorrect position.")
+      t.FailNow()
+    }
+  }
+}
+
+func TestRandomShuffle(t *testing.T) {
+  newDeck      := deck.New(0)
+
+  shuffled     := false
+  orderedSuit  := 0
+  orderedValue := 0
+
+  // there is an infinitesimally small chance that the deck randomly sorts
+  // in the same order as created, but I'm not going to account for that.
+  shuffledDeck := deck.Shuffle(newDeck, "random")
+  for _, card := range shuffledDeck {
+    if (card.Suit != deck.Suits[orderedSuit] || card.Value != orderedValue) {
+      shuffled = true
+      break
+    } else {
+      shuffled = false
+    }
+
+
+    orderedValue = (orderedValue + 1) % 13
+
+    // move to next suit if expectedValue wrapped around to 0
+    if orderedValue == 0 {
+      orderedSuit++
+    }
+  }
+
+  if shuffled != true {
+    t.Errorf("Deck was not shuffled randomly.")
   }
 }
