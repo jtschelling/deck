9c43fd3225d657f1d1a469ed98d5508a6bc9fe19
jtschelling <jt@schelling.io>
2020-09-12 18:45:22 -0500

    implement and test card removal

--- students/jtschelling/pkg/deck/deck.go
+++ students/jtschelling/pkg/deck/deck.go
@@ -10,20 +10,68 @@ type Card struct {
   Suit string
 }
 
-var cardTypes = [13]string{
-  "Ace",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "Jack",
-  "Queen",
-  "King",
+type CardType struct {
+  name string
+  Value int
+}
+
+var CardTypes = []CardType{
+  CardType {
+    name: "Ace",
+    Value: 0,
+  },
+  CardType {
+    name: "2",
+    Value: 1,
+  },
+  CardType {
+    name: "3",
+    Value: 2,
+  },
+  CardType {
+    name: "4",
+    Value: 3,
+  },
+  CardType {
+    name: "5",
+    Value: 4,
+  },
+  CardType {
+    name: "6",
+    Value: 5,
+  },
+  CardType {
+    name: "7",
+    Value: 6,
+  },
+  CardType {
+    name: "8",
+    Value: 7,
+  },
+  CardType {
+    name: "9",
+    Value: 8,
+  },
+  CardType {
+    name: "10",
+    Value: 9,
+  },
+  CardType {
+    name: "Jack",
+    Value: 10,
+  },
+  CardType {
+    name: "Queen",
+    Value: 11,
+  },
+  CardType {
+    name: "King",
+    Value: 12,
+  },
+  CardType {
+    name: "Joker",
+    Value: -1,
+  },
 }
 
 var StandardDeckSize = 52
@@ -35,15 +83,62 @@ var Suits = [4]string{
   "spade",
 }
 
-func New(numJokers int) []Card {
-  deck := make([]Card, (StandardDeckSize + numJokers))
+func CreateCardTypes(removedCards []string, jokerIncluded bool) []CardType {
+  if (removedCards == nil && jokerIncluded == false) {
+    return CardTypes[0:12]
+  } else if removedCards == nil {
+    return CardTypes
+  }
 
+  var includedCardTypes []CardType
+  for i := 0; i < len(CardTypes); i++ {
+    if !CardTypeExcluded(removedCards, CardTypes[i].name) {
+      if CardTypes[i].name == "Joker" && !jokerIncluded {
+        continue
+      }
+
+      includedCardTypes = append(includedCardTypes, CardTypes[i])
+    }
+  }
+
+  return includedCardTypes
+}
+
+func CardTypeExcluded(removedCards []string, cardType string) bool {
+  for _, cardName := range removedCards {
+    if cardName == cardType {
+      return true
+    }
+  }
+
+  return false
+}
+
+func New(removedCards []string, numJokers int) []Card {
   cardPosition := 0
 
+  jokerIncluded := false
+  if numJokers > 0 {
+    jokerIncluded = true
+  }
+  includedCardTypes := CreateCardTypes(removedCards, jokerIncluded)
+
+  numNonJockerCards := len(Suits) * (len(includedCardTypes))
+  if jokerIncluded {
+    numNonJockerCards = len(Suits) * (len(includedCardTypes) - 1)
+  }
+
+  deck := make([]Card, (numNonJockerCards + numJokers))
+
   for _, suit := range Suits {
-    for i := 0; i <= 12; i++ {
+    for _, cardType := range includedCardTypes {
+      // add jokers in the next for loop because they have a special value and no suit
+      if jokerIncluded && cardType.name == "Joker" {
+        continue
+      }
+
       deck[cardPosition] = Card {
-        Value: i,
+        Value: cardType.Value,
         Suit: suit,
       }
       cardPosition++
@@ -51,7 +146,7 @@ func New(numJokers int) []Card {
   }
 
   for i := 0; i < numJokers; i++ {
-    deck[StandardDeckSize + i] = Card {
+    deck[len(deck) - numJokers + i] = Card {
       Value: -1,
       Suit: "",
     }
--- students/jtschelling/test/deck_test.go
+++ students/jtschelling/test/deck_test.go
@@ -5,15 +5,37 @@ import (
   "github.com/jtschelling/deck/students/jtschelling/pkg/deck"
 )
 
-func testSortedDeck(testDeck []deck.Card) bool {
+func TestCardTypeExcluded(t *testing.T) {
+  shouldBeExcluded := "Ace"
+  if !deck.CardTypeExcluded([]string{shouldBeExcluded}, "Ace") {
+    t.Errorf("%s should have been excluded", shouldBeExcluded)
+  }
+
+  if deck.CardTypeExcluded([]string{}, "King") {
+    t.Errorf("%s should not have been excluded", "King")
+  }
+}
+
+func TestCreateCardTypes(t *testing.T) {
+  cardTypesRemoved := []string{
+    "Ace",
+  }
+
+  cardTypes := deck.CreateCardTypes(cardTypesRemoved, false)
+  if len(cardTypes) != 12 {
+    t.Errorf("card not removed")
+  }
+}
+
+func testSortedDeck(testDeck []deck.Card, deckSize int, numJokers int) bool {
   expectedSuit  := 0
   expectedValue := 0
 
   for _, card := range testDeck {
-    if (card.Suit != deck.Suits[expectedSuit] || card.Value != expectedValue) {
+    if (card.Suit != deck.Suits[expectedSuit] || card.Value != deck.CardTypes[card.Value].Value) {
       return false
     }
-    expectedValue = (expectedValue + 1) % 13
+    expectedValue = (expectedValue + 1) % ((deckSize - numJokers) / len(deck.Suits))
 
     // move to next suit if expectedValue wrapped around to 0
     if expectedValue == 0 {
@@ -21,8 +43,16 @@ func testSortedDeck(testDeck []deck.Card) bool {
 
       // if jokers are in the deck we need to break when all the standard suits
       // have been exhausted
-      if expectedSuit == 4 {
+      if expectedSuit == len(deck.Suits) {
         break
+      }
+    }
+  }
+
+  if numJokers > 0 {
+    for i := 0; i < numJokers; i++ {
+      if (testDeck[(deckSize - numJokers) + i].Suit != "" || testDeck[(deckSize - numJokers) + i].Value != -1) {
+        return false
       }
     }
   }
@@ -32,39 +62,58 @@ func testSortedDeck(testDeck []deck.Card) bool {
 
 func TestNew(t *testing.T) {
   numJokers := 0
-  newDeck := deck.New(numJokers)
+  newDeck := deck.New([]string{}, numJokers)
 
   if len(newDeck) != deck.StandardDeckSize + numJokers {
     t.Errorf("Deck is not %d cards", deck.StandardDeckSize + numJokers)
   }
 
-  if testSortedDeck(newDeck) == false {
+  if testSortedDeck(newDeck, len(newDeck), numJokers) == false {
+    t.Errorf("New unsorted deck is not in the correct order.")
+  }
+}
+
+func TestNewWithRemoved(t *testing.T) {
+  numJokers := 0
+  newDeck := deck.New([]string{}, numJokers)
+
+  if len(newDeck) != deck.StandardDeckSize + numJokers {
+    t.Errorf("Deck is not %d cards", deck.StandardDeckSize + numJokers)
+  }
+
+  if testSortedDeck(newDeck, len(newDeck), numJokers) == false {
+    t.Errorf("New unsorted deck is not in the correct order.")
+  }
+}
+
+func TestCreateEuchreDeck(t *testing.T) {
+  numJokers := 1
+  newDeck := deck.New([]string{"2", "3", "4", "5", "6", "7", "8"}, numJokers)
+
+  if len(newDeck) != (25) {
+    t.Errorf("Deck is not %d cards", 25)
+  }
+
+  if testSortedDeck(newDeck, len(newDeck), numJokers) == false {
     t.Errorf("New unsorted deck is not in the correct order.")
   }
 }
 
 func TestNewWithJokers(t *testing.T) {
   numJokers := 5
-  newDeck := deck.New(numJokers)
+  newDeck := deck.New([]string{}, numJokers)
 
   if len(newDeck) != (deck.StandardDeckSize + numJokers) {
     t.Errorf("Deck is not %d cards", (deck.StandardDeckSize + numJokers))
   }
 
-  if testSortedDeck(newDeck) == false {
+  if testSortedDeck(newDeck, len(newDeck), numJokers) == false {
     t.Errorf("New unsorted deck is not in the correct order.")
-  }
-
-  for i := 0; i < numJokers; i++ {
-    if (newDeck[deck.StandardDeckSize + i].Suit != "" || newDeck[deck.StandardDeckSize + i].Value != -1) {
-      t.Errorf("New unsorted deck does not contain jokers or are in the incorrect position.")
-      t.FailNow()
-    }
   }
 }
 
 func TestRandomShuffle(t *testing.T) {
-  newDeck      := deck.New(0)
+  newDeck      := deck.New([]string{}, 0)
 
   shuffled     := false
   orderedSuit  := 0
